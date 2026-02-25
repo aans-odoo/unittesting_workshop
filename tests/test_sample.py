5344dd9c0297f5be0d2f94e24e63917ce8d0bd56
Arib Ansari <aans@odoo.com>
2026-02-25 10:08:08 +0530

    rename sample test cases to be unique

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -4,17 +4,17 @@ from odoo.tests import tagged
 
 @tagged('demo')
 class TestSampleClass(TransactionCase):
-    def test_01(self):
+    def test_001(self):
         self.assertEqual(1, 1)
     
 
 @tagged('demo')
 class TestSampleClass02(TransactionCase):
-    def test_02(self):
+    def test_002(self):
         self.assertEqual(1, 1)
 
-    def test_03(self):
+    def test_003(self):
         self.assertEqual(1, 1)
     
-    def test_04(self):
+    def test_004(self):
         self.assertEqual(1, 1)
